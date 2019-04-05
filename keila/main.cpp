488381e606eb16d0051d2107cd3b686f07e9f621
Heyab Woldegebriel <heyabwoldegebriel@heyabs-macbook-pro.local>
2019-04-05 15:28:18 -0700

    Initial Commit

---
+++ keila/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  keila
+//
+//  Created by Heyab Woldegebriel on 4/5/19.
+//  Copyright © 2019 h. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
